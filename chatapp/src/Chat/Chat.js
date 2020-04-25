7cd1891d9091ef86df72785aca127c18ad16c634
Devina <devinardya@gmail.com>
2020-04-25 15:31:07 +0200

    fixed css look

--- chatapp/src/Chat/Chat.js
+++ chatapp/src/Chat/Chat.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import io from 'socket.io-client';
 import { DataMessagesHistory } from '../socket';
+import '../Chat/chat.scss';
 
 let socket = io('localhost:3000');
 
@@ -52,29 +53,56 @@ const Chat = ({location}) => {
         
     }
 
-    return <>
-             <h1>Chat</h1>
-             <p>Hej, {name}</p>
-             <form onSubmit = {onSubmit}>
-                 <input onChange={onChange} value={input}></input>
-                 <button></button>
-             </form>
-             <div>
-            {messages.map(data => {
-                let pointKey;
-                if (data.username === name){
-                      pointKey = "messages-"+ Math.round(Math.random() * 99999999999);
-                }else {
-                      pointKey = data.id;
-                }
-                 //console.log(data)
-                 return <div key={pointKey}>
-                            <p>{data.username} : {data.content}</p>
-                        </div>
-             })
-             } 
-             </div>
-           </>
+    return <div className="block__chatPage">
+                <div className="block__chatPage__sidebar">
+                    <div className="block__chatPage__sidebar--userbox">
+                        <h2>Welcome, {name}</h2>
+                    </div>
+                    <div className="block__chatPage__sidebar--userlist">
+                        <h3>Room user</h3>
+                        <p>Nono</p>
+                        <p>Bebe</p>
+                        <p>Bubu</p>
+                        <p>Jo</p>
+                    </div>
+                    <div className="block__chatPage__sidebar--roomlist">
+                        <h3>Room list</h3>
+                        <p>General</p>
+                        <p>Outside school</p>
+                        <p>Random</p>
+                    </div>
+                </div>
+                <div className="block__chatPage__mainbar">
+                    <div className="block__chatPage__mainbar--chatbox">
+                        {messages.map(data => {
+                            let pointKey;
+                            let boxClassName;
+                            if (data.username === name){
+                                pointKey = "messages-"+ Math.round(Math.random() * 99999999999);
+                                boxClassName = "block__chatPage__mainbar--chatbox--message--sender"
+                            }else {
+                                pointKey = data.id;
+                                boxClassName = "block__chatPage__mainbar--chatbox--message--incoming"
+                            }
+                            //console.log(data)
+                            return <div className={boxClassName} key={pointKey}>
+                                        <div className="block__chatPage__mainbar--chatbox--message--image"></div>
+                                        <div className="block__chatPage__mainbar--chatbox--message--blockText">
+                                            <p className="block__chatPage__mainbar--chatbox--message--username">{data.username}</p>
+                                            <p className="block__chatPage__mainbar--chatbox--message--text">{data.content}</p>
+                                        </div>
+                                    </div>
+                            })
+                        } 
+                    </div>
+                    <div className="block__chatPage__mainbar--form">
+                        <form onSubmit = {onSubmit}>
+                            <input onChange={onChange} type="text" placeholder="Enter messages..."  value={input}></input>
+                            <button>Submit</button>
+                        </form>
+                    </div>
+                </div>
+           </div>
 }
 
 export default Chat;
--- chatapp/src/Login/Login.js
+++ chatapp/src/Login/Login.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { Redirect } from 'react-router-dom';
 import '../Login/login.scss';
 import { AiFillWechat } from "react-icons/ai";
+import { IoMdLogIn } from 'react-icons/io';
 
 
 const Login = () => {
@@ -35,14 +36,14 @@ const Login = () => {
     return <div className = "block__loginPage">
                 <form className = "block__loginPage--form" onSubmit= {onSubmit}>
                     <div className="block__loginPage--form-top">
-                        <span className="block__loginPage--login-icon"><AiFillWechat className ="icons" size="60px" color="white" /></span> 
+                        <span className="block__loginPage--login-icon"><AiFillWechat className ="icons" size="60px" color="orange" /></span> 
                         <h3>Welcome!</h3>
                         <h5>Please log in to join the chat room!</h5>
                     </div>
                     <div className="block__loginPage--form-bottom">
                         <input className="block__loginPage--input-box" type="text" placeholder="Username" onChange={onChange} value={username}/>
+                        <button className="block__loginPage--login-button"><IoMdLogIn className="block__loginPage--login-button--icons" size="40px"/></button>
                     </div>
-                    <button className="block__loginPage--login-button">Enter chat</button>
                 </form>
             </div>
 }
